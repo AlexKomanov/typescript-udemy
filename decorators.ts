d8e697c25ed8a614688b251f58d7d836b83a66c4
Alex <akomanov88@gmail.com>
2021-07-23 18:48:23 +0300

    AK - Added custom decorator and field decorator

--- decorators.ts
+++ decorators.ts
@@ -1,27 +1,34 @@
 class CarDecorator {
+  @fieldDecorator
   color: string = "white";
 
   get formattedColor(): string {
     return `This car color is ${this.color}.`;
   }
 
-  @logError
+  @logError("The car has crashed!")
   drive(): void {
     throw new Error();
     console.log("I am driving");
   }
 }
 
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-  const method = desc.value;
-
-  desc.value = function () {
-    try {
-      method();
-    } catch (error) {
-      console.log("Darn! The car is crashed!");
-    }
-  };
+function fieldDecorator(target: any, key: string) {
+  console.log(target);
+  console.log(key);
 }
 
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+
+    desc.value = function () {
+      try {
+        method();
+      } catch (error) {
+        console.log(errorMessage);
+      }
+    };
+  };
+}
 new CarDecorator().drive();
